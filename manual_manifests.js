e8d3df1ce23b8d8d3b3aa26e8c2923a2c70649a4
febycv <febycv@gmail.com>
2012-02-13 22:21:00 +0530

    Update manual_manifests.js

--- manual_manifests.js
+++ manual_manifests.js
@@ -881,6 +881,17 @@
             "all": "true"
         },
         "summary": "Triple protection for your Android device"
+    },
+    {
+        "developer": "febycv",
+        "icon": "http://www.android3tablet.com/logo.png",
+        "id": "creamedglacier",
+        "free": "true",
+        "manifest": "https://raw.github.com/febycv/creamed_glacier_app_settings/master/creamedglacier.js",
+        "roms": {
+            "glacier": "true"
+        },
+        "summary": "ICS AOSP Rom for MT4G"
     }
     ],
     "minversion": "5006"
